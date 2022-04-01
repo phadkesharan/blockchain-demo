857a1ec7d12946fda4dabd215f55e3dc1f1ec0ea
Sharan Phadke <phadkesharan@gmail.com>
2022-04-02 00:05:36 +0530

    add component navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { Link } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Navbar />
+    </div>
+
   )
 }
